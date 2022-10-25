c71dd8fa586cbd19043520d5e3d20680b04dab7f
Bintang Pradana Erlangga Putra <bintangpradana02@gmail.com>
2022-10-25 22:50:38 +0700

    feat(user): encrypt password on registration

--- pkg/users/usecase.go
+++ pkg/users/usecase.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"gitlab.com/bpradana/privy-pretest/pkg/domain"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // Declare user usecase struct
@@ -20,7 +21,16 @@ func NewUserUsecase(userRepository domain.UserRepository) *UserUsecase {
 
 // Function to register user
 func (u *UserUsecase) Register(user *domain.User) (*domain.User, error) {
-	_, err := u.userRepository.Create(user)
+	// Hash password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Println("[users] [usecase] error hashing password, err: ", err.Error())
+		return nil, err
+	}
+	user.Password = string(hashedPassword)
+
+	// Create user
+	_, err = u.userRepository.Create(user)
 	if err != nil {
 		log.Println("[users] [usecase] error creating user, err: ", err.Error())
 		return nil, err
