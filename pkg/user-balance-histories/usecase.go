59a2b2fe830454453b71291f586618631cdbeb17
Bintang Pradana Erlangga Putra <bintangpradana02@gmail.com>
2022-10-26 22:35:50 +0700

    feat(balance-history): add check function in balance history usecase

---
+++ pkg/user-balance-histories/usecase.go
@@ -0,0 +1,53 @@
+package userBalanceHistories
+
+import (
+	"log"
+
+	"gitlab.com/bpradana/privy-pretest/pkg/domain"
+)
+
+// Declare user balance history usecase struct
+type UserBalanceHistoryUsecase struct {
+	userRepository               domain.UserRepository
+	userBalanceRepository        domain.UserBalanceRepository
+	userBalanceHistoryRepository domain.UserBalanceHistoryRepository
+}
+
+// Constructor function to create new user balance history usecase
+func NewUserBalanceHistoryUsecase(
+	userRepository domain.UserRepository,
+	userBalanceRepository domain.UserBalanceRepository,
+	userBalanceHistoryRepository domain.UserBalanceHistoryRepository,
+) *UserBalanceHistoryUsecase {
+	return &UserBalanceHistoryUsecase{
+		userRepository:               userRepository,
+		userBalanceRepository:        userBalanceRepository,
+		userBalanceHistoryRepository: userBalanceHistoryRepository,
+	}
+}
+
+// Function to get user balance history by username
+func (u *UserBalanceHistoryUsecase) Check(username string) (*[]domain.UserBalanceHistory, error) {
+	// Get user by username
+	user, err := u.userRepository.GetByUsername(username)
+	if err != nil {
+		log.Println("[balances] [usecase] error getting user by username, err: ", err.Error())
+		return nil, err
+	}
+
+	// Get balance by user id
+	balance, err := u.userBalanceRepository.GetByUserID(user.ID)
+	if err != nil {
+		log.Println("[balances] [usecase] error getting balance by username, err: ", err.Error())
+		return nil, err
+	}
+
+	// Get balance history by balance id
+	histories, err := u.userBalanceHistoryRepository.GetByBalanceID(balance.ID)
+	if err != nil {
+		log.Println("[balances] [usecase] error getting balance history by balance id, err: ", err.Error())
+		return nil, err
+	}
+
+	return histories, nil
+}
