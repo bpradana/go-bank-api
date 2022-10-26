3ffec7b64f8198206b9ce46ac6e49b17b44af15f
Bintang Pradana Erlangga Putra <bintangpradana02@gmail.com>
2022-10-26 16:08:02 +0700

    feat(balance): add withdraw function in balance usecase

--- pkg/balances/usecase.go
+++ pkg/balances/usecase.go
@@ -1,6 +1,7 @@
 package balances
 
 import (
+	"errors"
 	"fmt"
 	"log"
 
@@ -58,6 +59,40 @@ func (u *BalanceUsecase) Deposit(username string, amount int) (*domain.Balance, 
 
 	// Update balance
 	balance.Balance += amount
+	balance.BalanceAchieve = fmt.Sprintf("Rp. %d", balance.Balance)
+	balance, err = u.balanceRepository.Update(balance.ID, balance)
+	if err != nil {
+		log.Println("[balances] [usecase] error updating balance, err: ", err.Error())
+		return nil, err
+	}
+
+	return balance, nil
+}
+
+// Function to withdraw balance
+func (u *BalanceUsecase) Withdraw(username string, amount int) (*domain.Balance, error) {
+	// Get user by username
+	user, err := u.userRepository.GetByUsername(username)
+	if err != nil {
+		log.Println("[balances] [usecase] error getting user by username, err: ", err.Error())
+		return nil, err
+	}
+
+	// Get balance by user id
+	balance, err := u.balanceRepository.GetByUserID(user.ID)
+	if err != nil {
+		log.Println("[balances] [usecase] error getting balance by username, err: ", err.Error())
+		return nil, err
+	}
+
+	// Check if balance is enough
+	if balance.Balance < amount {
+		log.Println("[balances] [usecase] balance is not enough")
+		return nil, errors.New("balance is not enough")
+	}
+
+	// Update balance
+	balance.Balance -= amount
 	balance.BalanceAchieve = fmt.Sprintf("Rp. %d", balance.Balance)
 	balance, err = u.balanceRepository.Update(balance.ID, balance)
 	if err != nil {
