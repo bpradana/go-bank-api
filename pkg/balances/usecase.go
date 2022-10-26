5ffb591f9b966eccb3fba40038dc8e67c5050b8c
Bintang Pradana Erlangga Putra <bintangpradana02@gmail.com>
2022-10-26 15:52:02 +0700

    feat(balance): add deposit function in balance usecase

--- pkg/balances/usecase.go
+++ pkg/balances/usecase.go
@@ -1,6 +1,7 @@
 package balances
 
 import (
+	"fmt"
 	"log"
 
 	"gitlab.com/bpradana/privy-pretest/pkg/domain"
@@ -33,6 +34,34 @@ func (u *BalanceUsecase) CheckBalance(username string) (*domain.Balance, error) 
 	balance, err := u.balanceRepository.GetByUserID(user.ID)
 	if err != nil {
 		log.Println("[balances] [usecase] error getting balance by username, err: ", err.Error())
+		return nil, err
+	}
+
+	return balance, nil
+}
+
+// Function to deposit balance
+func (u *BalanceUsecase) Deposit(username string, amount int) (*domain.Balance, error) {
+	// Get user by username
+	user, err := u.userRepository.GetByUsername(username)
+	if err != nil {
+		log.Println("[balances] [usecase] error getting user by username, err: ", err.Error())
+		return nil, err
+	}
+
+	// Get balance by user id
+	balance, err := u.balanceRepository.GetByUserID(user.ID)
+	if err != nil {
+		log.Println("[balances] [usecase] error getting balance by username, err: ", err.Error())
+		return nil, err
+	}
+
+	// Update balance
+	balance.Balance += amount
+	balance.BalanceAchieve = fmt.Sprintf("Rp. %d", balance.Balance)
+	balance, err = u.balanceRepository.Update(balance.ID, balance)
+	if err != nil {
+		log.Println("[balances] [usecase] error updating balance, err: ", err.Error())
 		return nil, err
 	}
 
